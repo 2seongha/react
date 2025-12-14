a17741c3d373e233ca648aa2849dd578e402a1b5
LEE SEONGHA <lsh1230@istn.co.kr>
2025-12-15 00:04:31 +0900

    d

--- src/pages/PersonalExpense.tsx
+++ src/pages/PersonalExpense.tsx
@@ -17,12 +17,15 @@ import PersonalExpenseAddModal from '../components/PersonalExpenseAddModal';
 import CachedImage from '../components/CachedImage';
 import { banknotesGlassIcon } from '../assets/images';
 import SearchHelpModal from '../components/SearchHelpModal';
-import { getStart } from '../stores/service';
+import { getSearchHelp, getStart } from '../stores/service';
 import { webviewToast } from '../webview';
 import _ from 'lodash';
 import { AnimatePresence, motion } from 'framer-motion';
 import LoadingIndicator from '../components/LoadingIndicator';
 import DatePickerModal from '../components/DatePicker';
+import { FormRef } from '../stores/types';
+import CustomInput from '../components/CustomInput';
+import dayjs from 'dayjs';
 
 const PersonalExpense: React.FC = () => {
   const router = useIonRouter();
@@ -78,7 +81,7 @@ const PersonalExpense: React.FC = () => {
   // 항목 추가
   const handleAddItem = useCallback(() => {
     setDocItem(_.cloneDeep(oriItem.current));
-    addItemModalRef.current?.present();
+    setStep(99);
   }, []);
 
   const title = useMemo(() => {
@@ -89,6 +92,9 @@ const PersonalExpense: React.FC = () => {
         break;
       case 1:
         title = '헤더 설정';
+        break;
+      case 99:
+        title = '항목 추가';
         break;
     }
     return <AnimatePresence mode="wait">
@@ -125,7 +131,7 @@ const PersonalExpense: React.FC = () => {
             />
           )}
 
-          {step !== 0 && (
+          {step !== 0 && step !== 99 && (
             <IonButton
               mode="md"
               fill="clear"
@@ -137,6 +143,21 @@ const PersonalExpense: React.FC = () => {
               }}
             >
               <span style={{ fontSize: '16px', fontWeight: '600' }}>이전</span>
+            </IonButton>
+          )}
+
+          {step === 99 && (
+            <IonButton
+              mode='md'
+              fill="clear"
+              onClick={() => setStep(0)}
+              style={{
+                '--border-radius': '24px',
+                marginLeft: '8px',
+                width: '64px',
+              }}
+            >
+              <span style={{ fontSize: '16px', fontWeight: '600' }}>취소</span>
             </IonButton>
           )}
         </motion.div>
@@ -217,6 +238,9 @@ const PersonalExpense: React.FC = () => {
             </IonButton>
           </motion.div>}
 
+          {/* 항목 추가 페이지 */}
+          {step === 99 && <AddItem docItem={docItem} />}
+
           {/* 헤더 페이지 */}
           {step === 1 && <motion.div
             key="step1"
@@ -236,10 +260,10 @@ const PersonalExpense: React.FC = () => {
         </AnimatePresence>
 
         {/* 항목 추가 모달 */}
-        <PersonalExpenseAddModal
+        {/* <PersonalExpenseAddModal
           modalRef={addItemModalRef}
           docItem={docItem}
-        />
+        /> */}
         {/* 서치 헬프 모달 */}
         <SearchHelpModal />
 
@@ -288,3 +312,309 @@ const PersonalExpense: React.FC = () => {
 };
 
 export default PersonalExpense;
+
+interface AddItemProps {
+  docItem?: any;
+}
+
+const AddItem: React.FC<AddItemProps> = ({
+  docItem,
+}) => {
+  const historyPushedRef = useRef(false);
+  const closedByBackButtonRef = useRef(false);
+  // const modalRef = useRef<HTMLIonModalElement>(null);
+  const containerRef = useRef<HTMLDivElement>(null);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [isSaveEnabled, setIsSaveEnabled] = useState(false);
+  const formRef = useRef<FormRef>({});
+  const [, forceRender] = useState(0);
+  const modalId = 'addItem';
+
+  // docItem 변경 시 form 재할당
+  useEffect(() => {
+    formRef.current = docItem || {};
+    forceRender(prev => prev + 1);
+  }, [docItem]);
+
+  const checkRequired = useCallback(() => {
+    if (formRef.current.ACCOUNT_CODE_T
+      && formRef.current.WRBTR
+      && formRef.current.SGTXT) {
+      setIsSaveEnabled(true);
+    } else {
+      setIsSaveEnabled(false);
+    }
+  }, []);
+
+  const handleFocus = (e: any) => {
+    document.querySelectorAll('.has-focus').forEach((el) => {
+      el.classList.remove('has-focus');
+    });
+
+    const container = containerRef.current;
+    const el = e.target;
+    if (!container || !el) return;
+
+    setTimeout(() => {
+      const offset = 60; // 위에서 50px 밑으로
+      const containerTop = container.getBoundingClientRect().top;
+      const elTop = e.target.getBoundingClientRect().top;
+      const scrollTop = container.scrollTop + (elTop - containerTop) - offset;
+      container.scrollTo({
+        top: scrollTop,
+        behavior: "smooth",
+      });
+    }, 150)
+  };
+  const variants = {
+    enter: { y: "5%", opacity: 0 },   // 화면 밑에서 시작
+    center: { y: 0, opacity: 1 },       // 원래 위치
+    exit: { y: "5%", opacity: 0 }     // 다시 밑으로
+  };
+  return (
+    <motion.div
+      key={docItem + 'item'}
+      // custom={direction}
+      variants={variants}
+      initial="enter"
+      animate="center"
+      exit="exit"
+      transition={{ duration: 0.2 }}
+      ref={containerRef}
+      style={{
+        height: '100%',
+        overflow: 'auto',
+        paddingBottom: 'calc(102px + max(var(--ion-safe-area-bottom), var(--keyboard-height)))'
+      }}>
+      <CustomInput
+        formRef={formRef}
+        value="$ACCOUNT_CODE_T"
+        helperText="GL계정 : $SAKNR | GL계정명 : $SAKNR_T"
+        label="계정그룹명"
+        required
+        onFocus={handleFocus}
+        onValueHelp={() => getSearchHelp('ACCOUNT_CODE_T', 'IA103')}
+        onChange={(value) => {
+          formRef.current.ACCOUNT_CODE_T = value;
+          if (!value) {
+            formRef.current.ACCOUNT_CODE = '';
+            formRef.current.SAKNR = '';
+            formRef.current.SAKNR_T = '';
+          }
+          checkRequired();
+        }}
+        onChangeValueHelp={(value) => {
+          formRef.current.ACCOUNT_CODE = value.Key;
+          formRef.current.ACCOUNT_CODE_T = value.Name;
+          formRef.current.SAKNR = value.Add1;
+          formRef.current.SAKNR_T = value.KeyName;
+        }}
+        readOnly
+        clearInput
+        style={{ marginBottom: '28px' }}
+      />
+      <CustomInput
+        formRef={formRef}
+        value="$KOSTL"
+        helperText="코스트센터명 : $KOSTL_T"
+        label="코스트센터"
+        onFocus={handleFocus}
+        onValueHelp={() => getSearchHelp('KOSTL', 'IA103')}
+        onChange={(value) => {
+          formRef.current.KOSTL = value;
+          if (!value) {
+            formRef.current.KOSTL_T = '';
+          }
+        }}
+        onChangeValueHelp={(value) => {
+          formRef.current.KOSTL = value.Key;
+          formRef.current.KOSTL_T = value.Name;
+        }}
+        style={{ marginBottom: '28px' }}
+        clearInput
+      />
+      <CustomInput
+        formRef={formRef}
+        value="$AUFNR"
+        helperText="오더명 : $AUFNR_T"
+        label="오더번호"
+        onFocus={handleFocus}
+        onValueHelp={() => getSearchHelp('AUFNR', 'IA103')}
+        onChange={(value) => {
+          formRef.current.AUFNR = value;
+          if (!value) {
+            formRef.current.AUFNR_T = '';
+          }
+        }}
+        onChangeValueHelp={(value) => {
+          formRef.current.AUFNR = value.Key;
+          formRef.current.AUFNR_T = value.Name;
+        }}
+        style={{ marginBottom: '28px' }}
+        clearInput
+      />
+      <CustomInput
+        formRef={formRef}
+        value="$PROJK"
+        helperText="WBS요소명 : $PROJK_T"
+        label="WBS요소"
+        onFocus={handleFocus}
+        onValueHelp={() => getSearchHelp('PROJK', 'IA103')}
+        onChange={(value) => {
+          formRef.current.PROJK = value;
+          if (!value) {
+            formRef.current.PROJK_T = '';
+          }
+        }}
+        onChangeValueHelp={(value) => {
+          formRef.current.PROJK = value.Key;
+          formRef.current.PROJK_T = value.Name;
+        }}
+        style={{ marginBottom: '28px' }}
+        clearInput
+      />
+      <div style={{ display: 'flex', alignItems: 'center', gap: '12px', color: 'var(--ion-color-step-600)' }}>
+        <CustomInput
+          currency
+          formRef={formRef}
+          value="$WRBTR"
+          label="전표통화금액"
+          required
+          formatter={(value) => {
+            if (!value) return "";
+
+            const raw = value
+              .replace(/[^0-9\-]/g, "") // 숫자, - 만 허용
+              .replace(/(?!^)-/g, "");  // - 는 맨 앞만
+
+            if (raw === "" || raw === "-") return raw;
+
+            return Number(raw).toLocaleString("ko-KR");
+          }}
+          onFocus={handleFocus}
+          onChange={(value) => {
+            formRef.current.WRBTR = String(Math.trunc(Number(value)));
+            checkRequired();
+          }}
+          style={{ marginBottom: '28px', textAlign: 'right' }}
+          inputMode='numeric'
+        />
+        <span style={{ paddingBottom: '10px' }}>{docItem?.WAERS}</span>
+      </div>
+
+      <div style={{ display: 'flex', alignItems: 'center', gap: '12px', color: 'var(--ion-color-step-600)' }}>
+        <CustomInput
+          currency
+          formRef={formRef}
+          value="$DMBTR"
+          label="현지통화금액"
+          formatter={(value) => {
+            if (!value) return "";
+
+            const raw = value
+              .replace(/[^0-9\-]/g, "") // 숫자, - 만 허용
+              .replace(/(?!^)-/g, "");  // - 는 맨 앞만
+
+            if (raw === "" || raw === "-") return raw;
+
+            return Number(raw).toLocaleString("ko-KR");
+          }}
+          onFocus={handleFocus}
+          onChange={(value) => {
+            formRef.current.DMBTR = String(Math.trunc(Number(value)));
+          }}
+          style={{ marginBottom: '28px', textAlign: 'right' }}
+          inputMode='numeric'
+        />
+        <span style={{ paddingBottom: '10px' }}>{docItem?.HWAER}</span>
+      </div>
+
+      <CustomInput
+        formRef={formRef}
+        value="$SGTXT"
+        label="항목텍스트"
+        required
+        onFocus={handleFocus}
+        onChange={(value) => {
+          formRef.current.SGTXT = value;
+          checkRequired();
+        }}
+        style={{ marginBottom: '28px' }}
+        clearInput
+      />
+      <CustomInput
+        formRef={formRef}
+        value="$ZUONR"
+        label="지정"
+        onFocus={handleFocus}
+        onChange={(value) => {
+          formRef.current.ZUONR = value;
+        }}
+        style={{ marginBottom: '28px' }}
+        clearInput
+      />
+      <CustomInput
+        formRef={formRef}
+        value="$VALUT"
+        label="기준일자"
+        readOnly
+        date
+        formatter={(value) => {
+          return dayjs(value).format('YYYY-MM-DD');
+        }}
+        onFocus={handleFocus}
+        onChange={(value) => {
+          formRef.current.VALUT = value;
+        }}
+        style={{ marginBottom: '28px' }}
+      />
+      <CustomInput
+        formRef={formRef}
+        value="$ZFBDT"
+        label="만기계산일"
+        readOnly
+        date
+        formatter={(value) => {
+          return dayjs(value).format('YYYY-MM-DD');
+        }}
+        onFocus={handleFocus}
+        onChange={(value) => {
+          formRef.current.ZFBDT = value;
+        }}
+        style={{ marginBottom: '28px' }}
+      />
+      <div
+        style={{
+          position: 'fixed',
+          left: 0,
+          bottom: 0,
+          width: "100%",
+          display: "flex",
+          padding: "12px 21px",
+          zIndex: '2',
+          paddingTop: '32px',
+          background: 'linear-gradient(to top, var(--ion-background-color) 0%, var(--ion-background-color) calc(100% - 20px), transparent 100%)',
+          paddingBottom: 'calc(12px + max(var(--ion-safe-area-bottom), var(--keyboard-height)))',
+          transform: 'translateZ(0)'
+        }}
+      >
+        <IonButton
+          mode="md"
+          color="primary"
+          style={{
+            flex: 1,
+            height: "58px",
+            fontSize: "18px",
+            fontWeight: "600",
+          }}
+          disabled={!isSaveEnabled}
+          onClick={() => {
+            // dismiss();
+          }}
+        >
+          <span>저장</span>
+        </IonButton>
+      </div>
+    </motion.div>
+  );
+};
